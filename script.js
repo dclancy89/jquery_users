cb76d0c98cb2dc8a2d4f3f9ba7e232ff2cde7e2c
Daniel <clancy.daniel@gmail.com>
2018-02-08 14:06:25 -0600

    added a table and a form to add users to the table

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(document).ready(function() {
+
+	$('#add_users').submit(function(e){
+		e.preventDefault();
+		var row = '<tr>';
+		row += '<td>' + $('#first_name').val() + '</td>';
+		row += '<td>' + $('#last_name').val() + '</td>';
+		row += '<td>' + $('#email').val() + '</td>';
+		row += '<td>' + $('#phone').val() + '</td>';
+
+		row += '</tr>';
+
+		$('#user_table tbody').append(row);
+		$('#add_user').children('label').children('input[type="text"]').val('');
+	});
+
+
+
+})
